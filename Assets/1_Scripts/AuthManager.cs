780fda955ac9c7de642bedf5ddfc7ad466d15124
Cafor <formuloratio@gmail.com>
2026-01-14 18:31:23 +0900

    feat: 회원가입 및 로그인 기능

--- Assets/1_Scripts/AuthManager.cs
+++ Assets/1_Scripts/AuthManager.cs
@@ -1,30 +1,119 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using System;
 
 public class AuthManager : MonoBehaviour
 {
     [SerializeField] private Button loginButton;
     [SerializeField] private Button logoutButton;
+    [SerializeField] private Button signUpButton;
+
+    [SerializeField] private TMP_InputField useNameIF_SignUp;
+    [SerializeField] private TMP_InputField passwordIF_SignUp;
+    [SerializeField] private TMP_InputField useNameIF_Login;
+    [SerializeField] private TMP_InputField passwordIF_Login;
+
+    public static event Action OnSignUpSuccess;
+    public static event Action OnLoginSuccess;
 
     private async void Awake()
     {
-        UnityServices.Initialized += () => { Debug.Log("UGS 초기화 완료"); };
+        UnityServices.Initialized += () =>
+        {
+            Debug.Log("UGS 초기화 완료");
+        };
 
         // UGS 초기화
         await UnityServices.InitializeAsync(); //초기화 작업될 때까지 대기
 
-        //버튼 이벤트 연결
-        loginButton.onClick.AddListener(OnLoginButtonClicked);
+        // 인증관련 이벤트 연결
+        EventBinding();
+
+        // 회원가입
+        signUpButton.onClick.AddListener( async () =>
+        {
+            var userName = useNameIF_SignUp.text;
+            var password = passwordIF_SignUp.text;
+            try
+            {
+                await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(userName, password);
+                OnSignUpSuccess?.Invoke();
+                AuthenticationService.Instance.SignOut(); //회원가입 후 자동로그인 방지 위해 로그아웃 처리
+                Debug.Log("회원가입 성공");
+            }
+            catch (AuthenticationException e)
+            {
+                Debug.LogError("회원가입 실패: " + e.Message);
+            }
+            catch (RequestFailedException e)
+            {
+                Debug.LogError("회원가입 실패: " + e.Message);
+            }
+        });
+
+        //로그인
+        loginButton.onClick.AddListener( async () =>
+        {
+            var userName = useNameIF_Login.text;
+            var password = passwordIF_Login.text;
+            try
+            {
+                await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(userName, password);
+                OnLoginSuccess?.Invoke();
+                Debug.Log("로그인 성공");
+            }
+            catch (AuthenticationException e)
+            {
+                Debug.LogError("로그인 실패: " + e.Message);
+            }
+            catch (RequestFailedException e)
+            {
+                Debug.LogError("로그인 실패: " + e.Message);
+            }
+        });
+
+        //로그아웃
+        logoutButton.onClick.AddListener( () =>
+        {
+            AuthenticationService.Instance.SignOut();
+        });
     }
 
-    private async void OnLoginButtonClicked()
+    private void EventBinding()
     {
-        // 익명 로그인 요청
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        // 로그인 성공
+        AuthenticationService.Instance.SignedIn += async () =>
+        {
+            Debug.Log("로그인 성공! 플레이어 ID: " + AuthenticationService.Instance.PlayerId);
+
+            // 플레이어 이름 가져오기
+            await AuthenticationService.Instance.GetPlayerNameAsync();
+            var playerName = AuthenticationService.Instance.PlayerName;
+        };
+
+        // 로그아웃
+        AuthenticationService.Instance.SignedOut += () =>
+        {
+            Debug.Log("로그아웃 되었습니다.");
+        };
+
+        //로그인 실패
+        AuthenticationService.Instance.SignInFailed += (err) =>
+        {
+            Debug.Log("로그인 실패: " + err);
+        };
+
+        //세션 아웃
+        AuthenticationService.Instance.Expired += () =>
+        {
+            Debug.Log("세션이 만료되었습니다.");
+        };
     }
+
 }
---
+++ Assets/1_Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject SignupUI;
+    [SerializeField] private GameObject LoginUI;
+
+    [SerializeField] private Button SignUpClosedButton;
+
+    private void Awake()
+    {
+        SignUpClosedButton.onClick.AddListener(CloseSignUpUI);
+    }
+
+    private void OnEnable()
+    {
+        AuthManager.OnSignUpSuccess += OnClickSignUpButton;
+        AuthManager.OnLoginSuccess += OnClickLoginButton;
+    }
+
+    private void OnDisable()
+    {
+        AuthManager.OnSignUpSuccess -= OnClickSignUpButton;
+        AuthManager.OnLoginSuccess -= OnClickLoginButton;
+    }
+
+    private void OnClickSignUpButton()
+    {
+        SignupUI.SetActive(false);
+        LoginUI.SetActive(true);
+    }
+
+    private void OnClickLoginButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    private void CloseSignUpUI()
+    {
+        SignupUI.SetActive(false);
+        LoginUI.SetActive(true);
+    }
+}
